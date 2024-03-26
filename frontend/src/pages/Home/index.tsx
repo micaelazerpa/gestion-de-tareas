f6cc5fa77c68d4dc15f8a259326683915fcbdcd0
Micaela Zerpa <micazerpa14@gmail.com>
2024-03-25 23:16:05 -0300

    folder page

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Este es el home hijo de RootLayout</h1>
+    </main>
+  );
+}
