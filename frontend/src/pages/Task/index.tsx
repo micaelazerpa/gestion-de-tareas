dd433b46a1d2ef8e84c8a41a5f78d68901c8dbc8
Micaela Zerpa <micazerpa14@gmail.com>
2024-04-17 11:23:21 -0300

    show

--- frontend/src/pages/Task/index.tsx
+++ frontend/src/pages/Task/index.tsx
@@ -1,19 +1,27 @@
-'use client'
+"use client";
 import { useTaskProvider } from "@/app/hooks/useTaskProvider";
-import {ChevronRightIcon, PencilIcon, DocumentTextIcon } from "@heroicons/react/24/outline";
+import {
+  ChevronRightIcon,
+  PencilIcon,
+  DocumentTextIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
 
 export default function TaskPage(params: any) {
   const { task } = params;
   const router = useRouter();
-  const {setTasks} = useTaskProvider() 
- 
+  const { setTasks } = useTaskProvider();
+  const [open, setOpen] = useState(false);
+
   //console.log("parametros en tarjet", task);
-  const handleClick = ()=>{
-    setTasks(task)
-    router.push(`/task/${task._id}`)
-  }
+  const handleClick = () => {
+    setTasks(task);
+    router.push(`/task/${task._id}`);
+  };
   return (
     <div className="mt-5 px-4 w-full sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/6">
       <div className="overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
@@ -25,25 +33,72 @@ export default function TaskPage(params: any) {
                 aria-hidden="true"
               />
             </div> */}
-            
+
             <div className="h-full flex flex-col">
-              <h2 className="flex font-semibold text-gray-900 h-auto">{task.nombre}</h2>
-              <p className="flex flex-grow mt-1 text-gray-600 text-clip overflow-hidden">{task.descripcion}</p>
+              <h2 className="flex font-semibold text-gray-900 h-auto">
+                {task.nombre}
+              </h2>
+              <p className="flex flex-grow mt-1 text-gray-600 text-clip overflow-hidden">
+                {task.descripcion}
+              </p>
             </div>
           </div>
           <p className="mt-1 text-gray-600 h-1/6 self-center">{task.estado}</p>
         </div>
         <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-          <Link
-            href="/"
-            className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
+          <button
+            className="w-full flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
+            onClick={() => setOpen(true)}
           >
             <ChevronRightIcon
               className="h-5 w-5 flex-none text-gray-400"
               aria-hidden="true"
             />
             Ver
-          </Link>
+          </button>
+          <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={setOpen}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block" />
+              </Transition.Child>
+              <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+                  <Dialog.Panel className="flex w-1/2 transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                    <div className="rounded-lg relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                      <button
+                        type="button"
+                        className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
+                        onClick={() => setOpen(false)}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+
+                      <div className="h-full w-full flex flex-col">
+                        <h2 className="flex font-semibold text-gray-900 h-auto">
+                          {task.nombre}
+                        </h2>
+                        <p className="flex flex-grow mt-1 text-gray-600 text-clip overflow-hidden">
+                          {task.descripcion}
+                        </p>
+                        <p className="flex mt-8 text-gray-600 h-1/6 self-center">
+                          {task.estado}
+                        </p>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </div>
+              </div>
+            </Dialog>
+          </Transition.Root>
           <button
             //href={`/[taskId]/${task.id}`}
             //href="/task/[id]" as={`/task/${task._id}`}
